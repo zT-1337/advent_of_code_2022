d6d97f00bcd77d96a4785f5847d34893046c1d11
Thomas Zerr <thomas.zerr@gmail.com>
2022-12-07 21:05:08 +0100

    -added: second start of day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,7 @@
 use crate::util::load_lines_of_file;
 use std::io::BufRead;
 
+#[derive(Clone)]
 enum RpsChoice {
     Rock,
     Paper,
@@ -19,6 +20,19 @@ impl RpsChoice {
             _ => panic!("Unexpected from: {}", from),
         }
     }
+    
+    fn convert_from_opponent_and_result(opponent: &RpsChoice, result: &str) -> Self {
+        match (opponent, result) {
+            (RpsChoice::Rock, "X") => RpsChoice::Scissor,
+            (RpsChoice::Rock, "Z") => RpsChoice::Paper,
+            (RpsChoice::Paper, "X") => RpsChoice::Rock,
+            (RpsChoice::Paper, "Z") => RpsChoice::Scissor,
+            (RpsChoice::Scissor, "X") => RpsChoice::Paper,
+            (RpsChoice::Scissor, "Z") => RpsChoice::Rock,
+            (_, "Y") => opponent.clone(),
+            (_, _) => panic!("Unexpected result: {}", result),
+        }
+    }
 
     fn points_for_choice(&self) -> u32 {
         match self {
@@ -28,7 +42,7 @@ impl RpsChoice {
         }
     }
 
-    fn points_for_outcome(&self, opponent: RpsChoice) -> u32 {
+    fn points_for_outcome(&self, opponent: &RpsChoice) -> u32 {
         match (self, opponent) {
             (RpsChoice::Rock, RpsChoice::Paper) => 0,
             (RpsChoice::Rock, RpsChoice::Scissor) => 6,
@@ -60,8 +74,33 @@ pub fn day_2_star_1() {
         let myself = RpsChoice::convert_from(&line[2..3]);
 
         score += myself.points_for_choice();
-        score += myself.points_for_outcome(opponent);
+        score += myself.points_for_outcome(&opponent);
     }
 
-    println!("Result of Advent of Code Day 2, Start 1: {}", score);
+    println!("Result of Advent of Code Day 2, Star 1: {}", score);
+}
+
+pub fn day_2_star_2() {
+    let file = load_lines_of_file("/home/zt/Workspace/advent_of_code/src/day2.input");
+
+    let mut score: u32 = 0;
+
+    for line in file.lines() {
+        let line = match line {
+            Ok(value) => value,
+            Err(why) => panic!("Reading line failed: {}", why),
+        };
+
+        if line.len() != 3 {
+            break;
+        }
+
+        let opponent = RpsChoice::convert_from(&line[0..1]);
+        let myself = RpsChoice::convert_from_opponent_and_result(&opponent, &line[2..3]);
+
+        score += myself.points_for_choice();
+        score += myself.points_for_outcome(&opponent);
+    }
+
+    println!("Result of Advent of Code Day 2, Star 1: {}", score);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod day1;
 mod day2;
 mod util;
 
-use day2::day_2_star_1;
+use day2::{day_2_star_1, day_2_star_2};
 
 use crate::day1::{day1_star_1, day1_star_2};
 
@@ -11,4 +11,5 @@ fn main() {
     day1_star_2();
 
     day_2_star_1();
+    day_2_star_2();
 }
