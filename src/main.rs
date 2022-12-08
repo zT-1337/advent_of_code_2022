0f3583833d1a482dbf660672b7a6f96484228aa4
Thomas Zerr <thomas.zerr@gmail.com>
2022-12-08 13:14:19 +0100

    -added: star 2 of day3

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod day3;
 mod util;
 
 use day2::{day_2_star_1, day_2_star_2};
-use day3::day_3_star_1;
+use day3::{day_3_star_1, day_3_star_2};
 
 use crate::day1::{day1_star_1, day1_star_2};
 
@@ -16,4 +16,5 @@ fn main() {
     day_2_star_2();
 
     day_3_star_1();
+    day_3_star_2();
 }
